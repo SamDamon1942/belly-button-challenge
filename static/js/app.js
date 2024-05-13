78a608116619eaf0f957c2648e68595cc2a8bdf2
SamDamon1942 <jbeezyIT@proton.me>
2024-05-12 21:10:57 -0600

    made changes to traces and functions

--- static/js/app.js
+++ static/js/app.js
@@ -6,23 +6,29 @@ function buildMetadata(sample) {
     let metadataField = data.metadata;
 
     // Assign the value of the dropdown menu option to a variable
-    let sampleNumber = dropdownMenu.property("value");
+    let dropdownMenu = d3.select("#selDataset");
+    let sampleNumber =dropdownMenu.value;
    
     // Filter the metadata for the object with the desired sample number
-    let filteredMetaData = metadataField.filter(obj => obj.id === sampleNumber);
+    let filteredMetaData = metadataField.filter(obj => obj.id === sample);
+    let arr_filtered = filteredMetaData[0];
+
+    console.log(arr_filtered);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-     let panel = d3.select("sample-metadata");
+     let PANEL = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-    panel.html("");
+    PANEL.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-    Object.entries(filteredMetadata).forEach(([key, value]) => {
-      // Append a new tag for each key-value pair
-      panel.append("p").text(`${key}: ${value}`);
-    });
+
+      Object.entries(arr_filtered).forEach(([key, value]) => {
+      
+        // Append a new tag for each key-value pair
+      PANEL.append("p").text(`${key}: ${arr_filtered[value]}`);
+      });
   });
 }
 
@@ -34,33 +40,34 @@ function buildCharts(sample) {
     let samplesData = data.samples;
 
     // Filter the samples for the object with the desired sample number
-    let filteredSample = samplesData.filter(obj=> obj.id === sampleNumber);
+    let filteredSample = samplesData.filter(obj=> obj.id === sample);
+    let arr_filtered_sample = filteredSample[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-    let otu_ids = filteredSample.otu_ids;
-    let otu_labels = filteredSample.otu_labels;
-    let sample_values = filteredSample.sample_values;
+    let otu_ids = arr_filtered_sample.otu_ids;
+    let otu_labels = arr_filtered_sample.otu_labels;
+    let sample_values = arr_filtered_sample.sample_values;
 
     // Build a Bubble Chart
    let traceBubble = {
       x: otu_ids,
       y: sample_values,
       text: otu_labels,
-      mode: 'markers',
-      marker: {
+          marker: {
           size: sample_values,
           color: otu_ids,
           colorscale: "Earth"
       }
     };
+
     let layoutBubble = {
       title: 'Bubble Chart',
       xaxis: { title: 'OTU IDs' },
       yaxis: { title: 'Sample Values' }
     }; 
 
-    // Render the Bubble Chart
-    Plotly.newPlot('bubble-chart', [traceBubble], layoutBubble);
+      // Render the Bubble Chart
+      Plotly.newPlot('bubble', traceBubble, layoutBubble);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
     let yTicks = otu_ids.map(id => `OTU ${id}`);
@@ -70,7 +77,6 @@ function buildCharts(sample) {
     let traceBar = {
       x: sample_values,
       y: yTicks,
-      type: 'bar',
       orientation: 'h'
     };
 
@@ -79,9 +85,9 @@ function buildCharts(sample) {
       xaxis: { title: 'Sample Values' },
       yaxis: { title: 'OTU IDs' }
     };
-
+    
     // Render the Bar Chart
-    Plotly.newPlot('bar-chart', [traceBar], layoutBar);
+    Plotly.newPlot(bar, traceBar, layoutBar);
 
   });
 }
@@ -92,6 +98,7 @@ function init() {
 
     // Get the names field
     let sampleNames = data.names;
+    let samplesData = data.samples;
 
     // Use d3 to select the dropdown with id of `#selDataset`
     let dropdownMenu = d3.select("#selDataset");
@@ -100,18 +107,24 @@ function init() {
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
     sampleNames.forEach(name => {
-      dropdownMenu.append("option").text(name);
+      dropdownMenu.append("option").text(name).property("value",sampleNames[0]);
     });
 
     // Get the first sample from the list
-      let selectedSampleName = dropdownMenu.select(this).property("value");
-  
+      let selectedSampleName = dropdownMenu.property("value");
+      //console.log(selectedSampleName);
+      
     // Use the selected sample name to access the corresponding data
-      let selectedSampleData = data.filter(obj=> obj.id === selectedSampleName);
-         
+
+      //let samplesData = data.samples;
+      //let selectedSampleData = samplesData.filter(obj => obj.id === selectedSampleName);
+    
+      let firstSample = samplesData.filter(obj=> obj.id === selectedSampleName);
+      let arr_first_sample = firstSample[0];
+    
     // Build charts and metadata panel with the first sample
-     buildCharts(selectedSampleName);
-     buildMetadata(selectedSampleData);
+     buildCharts(firstSample);
+     buildMetadata(arr_first_sample);
   });
 }
 
